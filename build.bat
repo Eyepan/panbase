74bcafb0ca183331c8627e3eb02caecd0d0718f3
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-26 20:09:49 +0530

    requirements

---
+++ build.bat
@@ -0,0 +1,11 @@
+python -m nuitka main.py ^
+--output-dir=dist ^
+--onefile ^
+--standalone ^
+-o panbase.exe ^
+--quiet ^
+--remove-output ^
+--show-progress ^
+--include-data-file=database.db=database.db ^
+--include-data-dir=admin-ui/dist=admin-ui/dist ^
+--onefile-tempdir-spec="%TEMP%\panbase\0.1.0"
